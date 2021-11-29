5c54e9ad847418ed1bbc680fb24020732d0e6313
guptakartik113 <kg2072660@gmail.com>
2021-11-29 10:40:32 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "jquery";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router";
+import { Routes } from "react-router";
+import Home from "./components/home/Home";
+
+import ProductDetails from "./components/details/ProductDetails";
+import Error from "./components/404/Error";
+import Cart from "./components/cart/Cart";
+import PaymentOption from "./components/cart/PaymentOption";
+import ShoppingDetails from "./components/cart/ShoppingDetails";
+import ShoppingCart from "./components/cart/ShoppingCart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route
+            path="/productDetails/:id"
+            exact
+            element={<ProductDetails />}
+          />
+          <Route path="/paymentOption" exact element={<PaymentOption />} />
+          <Route path="/shoppingCart" exact element={<ShoppingCart />} />
+          <Route path="/shoppingDetails" exact element={<ShoppingDetails />} />
+          <Route path="/paymentOption" exact element={<PaymentOption />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
